b1f1ab7b46759ea0c903d6f77805ff269be8c2fe
Nicolas Marteau <nico_mart@hotmail.fr>
2019-04-09 18:08:32 +0200

    chore: replace console.log with throw

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 // Require Third-party dependencies
 const { get, post, send } = require("httpie");
-const { red } = require("kleur");
 
 // Constantes
 const PORT = 1337;
 const userURL = new URL(`http://localhost:${PORT}`);
-
-// Globals
-let accessToken = "";
 
 /**
  * @async
@@ -28,13 +24,12 @@ async function meta() {
  * @returns {Promise} Object of the request with access_token key
  */
 async function login(username, password) {
-    if (username === undefined || password === undefined) {
-        return console.error(red("This method need two arguments, username & password"));
+    if (typeof username !== "string" || typeof password !== "string") {
+        throw new TypeError("username and password must be strings");
     }
     const { data } = await post(new URL("/login", userURL), {
         body: { username, password }
     });
-    accessToken = data.access_token;
 
     return data;
 }
@@ -48,8 +43,8 @@ async function login(username, password) {
  * @returns {Promise} Object of the request with key userId
  */
 async function users(username, password) {
-    if (username === undefined || password === undefined) {
-        return console.error(red("This method need two arguments, username & password"));
+    if (typeof username !== "string" || typeof password !== "string") {
+        throw new TypeError("username and password must be strings");
     }
     const { data } = await post(new URL("/users", userURL), {
         body: { username, password }
@@ -73,8 +68,8 @@ async function users(username, password) {
  */
 // eslint-disable-next-line consistent-return
 async function publish(addonInfos, token) {
-    if (addonInfos === undefined || token === undefined) {
-        return console.error(red("This method need two arguments, addonInfos & token"));
+    if (!Object.keys(addonInfos).length || typeof token !== "string") {
+        throw new TypeError("addonInfos mustn't be a empty object, token must be a string");
     }
     const { data } = await post(new URL("/addon/publish", userURL), {
         body: { name, description, version, git, organisation } = addonInfos,
@@ -115,20 +110,20 @@ async function addonName(addonName) {
 async function test() {
     // console.log(await meta());
     // console.log(await login("nicolas", "NICOLAS"));
-    // const token = await login("nicolas", "NICOLAS");
-    // console.log(await publish({
-    //     name: "sdfdsfvrdsg",
-    //     description: "",
-    //     version: "1.0.0",
-    //     git: "http://test.fr"
-    // }, token.access_token));
+    const token = await login("nicolas", "NICOLAS");
+    console.log(await publish({
+        name: "test7",
+        description: "",
+        version: "1.0.0",
+        git: "http://test.fr"
+    }, token.access_token));
     // console.log(await users("Sophie", "parkerr"));
     // console.log(await addon());
-    console.log(await addonName("test3"));
+    // console.log(await addonName("test3"));
 }
 
 test();
 
-module.exports = { meta, login, users, publish, addon };
+module.exports = { meta, login, users, publish, addon, addonName };
 
 
