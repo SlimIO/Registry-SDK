b23eae86698b985d126e5ec0e87033568aa6a67d
Nicolas Marteau <nico_mart@hotmail.fr>
2019-04-10 10:37:09 +0200

    docs: change @returns

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { typeArg } = require("./src/utils");
 
 // Require Third-party dependencies
-const { get, post, send } = require("httpie");
+const { get, post } = require("httpie");
 
 // Constantes
 const PORT = 1337;
@@ -12,7 +12,7 @@ const userURL = new URL(`http://localhost:${PORT}`);
  * @async
  * @function meta
  * @description Return service metadata
- * @returns {Promise} Object of the request with uptime key
+ * @returns {Promise<Object>} Object of the request with uptime key
  */
 async function meta() {
     return (await get(userURL)).data;
@@ -24,7 +24,7 @@ async function meta() {
  * @description Authenticate a user and get an AccessToken.
  * @param {!string} username User name
  * @param {!string} password User password
- * @returns {Promise} Object of the request with access_token key
+ * @returns {Promise<Object>} Object of the request with access_token key
  */
 async function login(username, password) {
     if (typeArg(username, password)) {
@@ -43,7 +43,7 @@ async function login(username, password) {
  * @description Create a new user.
  * @param {!string} username User name
  * @param {!string} password User password
- * @returns {Promise} Object of the request with key userId
+ * @returns {Promise<Object>} Object of the request with key userId
  */
 async function users(username, password) {
     if (typeArg(username, password)) {
@@ -67,7 +67,7 @@ async function users(username, password) {
  * @param {!string} addonInfos.git Git url
  * @param {string} addonInfos.organisation Organisaion name
  * @param {!string} token Access token user
- * @returns {Promise} Object with addonId key
+ * @returns {Promise<Object>} Object with addonId key
  */
 // eslint-disable-next-line consistent-return
 async function publish(addonInfos, token) {
@@ -99,7 +99,7 @@ async function addon() {
  * @function addonName
  * @description Get a given addon by his name.
  * @param {!string} addonName Addon name
- * @returns {Promise} Object with addon infos
+ * @returns {Promise<Object>} Object with addon infos
  */
 async function addonName(addonName) {
     if (typeArg(addonName)) {
@@ -113,7 +113,7 @@ async function addonName(addonName) {
  * @async
  * @function orga
  * @description Get all organisations
- * @returns {Promise} Object with organisations infos
+ * @returns {Promise<Object>} Object with organisations infos
  */
 async function orga() {
     return (await get(new URL("/organisation", userURL))).data;
@@ -124,7 +124,7 @@ async function orga() {
  * @function orgaName
  * @description Get an organisation by his name
  * @param {!string} name Organisation name
- * @returns {Promise} Object with organisation infos
+ * @returns {Promise<Object>} Object with organisation infos
  */
 async function orgaName(name) {
     if (typeArg(name)) {
@@ -141,7 +141,7 @@ async function orgaName(name) {
  * @param {!string} orgaName Organisation name
  * @param {!string} userName User name
  * @param {!string} token User token
- * @returns {Promise} Object with organisation and user infos
+ * @returns {Promise<Object>} Object with organisation and user infos
  */
 async function addUser(orgaName, userName, token) {
     if (typeArg(orgaName, userName, token)) {
