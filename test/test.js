30865eed633188550c96aecb3cedd07a97b33a02
Nicolas Marteau <nico_mart@hotmail.fr>
2019-04-17 10:45:53 +0200

    test: add test meta()

--- test/test.js
+++ test/test.js
@@ -3,29 +3,28 @@ const assert = require("assert");
 
 // Require Third-Party dependencies
 const { get, post } = require("httpie");
+const japa = require("japa");
+const is = require("@slimio/is");
 
 // Internal dependencies
-const {
-    meta,
-    login,
-    users,
-    addon,
-    addonName,
-    publish,
-    orga,
-    orgaName,
-    orgaAddUser
-} = require("../index");
+const { meta, login, users, addon, addonName, publish, orga, orgaName, orgaAddUser } = require("../index");
 
 // Constantes
 const PORT = 1337;
 const REGISTRY_URL = new URL(`http://localhost:${PORT}`);
 
-describe("Methods tests", () => {
-    describe("#meta()", () => {
-        it("should return a uptime as JSON", async() => {
-            const data = await meta();
-            assert.deepEqual(Object.keys(data), ["uptime"], "meta() method only return 'uptime' key");
-        });
-    });
+japa("meta() should returned an Object with uptime Key", async(assert) => {
+    const retMeta = await meta();
+    assert.strictEqual(is.plainObject(retMeta), true);
+    assert.deepEqual(Object.keys(retMeta), ["uptime"]);
 });
+
+japa("login() should returned an error if no arguments", async(assert) => {
+    
+})
+
+japa("login() should returned a string", async(assert) => {
+    const retLogin = await login("admin1", "admin1953");
+    assert.strictEqual(is.string(retLogin), true);
+});
+
