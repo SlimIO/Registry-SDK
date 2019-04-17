2ce9d34b72db61c86f8717e2435ba53f89176bd1
Nicolas Marteau <nico_mart@hotmail.fr>
2019-04-17 11:26:23 +0200

    test: add users() tests

--- test/test.js
+++ test/test.js
@@ -31,7 +31,7 @@ japa("login() should returned an ArgumentError if no arguments", async(assert) =
 
 japa("login() should returned an ArgumentError if just one arguments", async(assert) => {
     try {
-        await login("admin");
+        await login("test");
     }
     catch (err) {
         assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
@@ -40,7 +40,7 @@ japa("login() should returned an ArgumentError if just one arguments", async(ass
 
 japa("login() should returned an ArgumentError if arguments aren't string", async(assert) => {
     try {
-        await login("admin", 50);
+        await login("test", 50);
     }
     catch (err) {
         assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
@@ -50,5 +50,40 @@ japa("login() should returned an ArgumentError if arguments aren't string", asyn
 japa("login() should returned a string", async(assert) => {
     const retLogin = await login("admin1", "admin1953");
     assert.strictEqual(is.string(retLogin), true);
+});
+
+japa("users() should returned an ArgumentError if no arguments", async(assert) => {
+    try {
+        await users();
+    }
+    catch (err) {
+        assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+    }
+});
+
+japa("users() should returned an ArgumentError if just one arguments", async(assert) => {
+    try {
+        await users("test");
+    }
+    catch (err) {
+        assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+    }
+});
+
+japa("users() should returned an ArgumentError if arguments aren't string", async(assert) => {
+    try {
+        await users("test", 50);
+    }
+    catch (err) {
+        assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+    }
+});
+
+japa("users() should returned an object with userId key (number)", async(assert) => {
+    const nbUserRandom = Math.floor(Math.random() * 10000);
+    const retUsers = await users(`admin${nbUserRandom}`, "admin1953");
+    assert.strictEqual(is.plainObject(retUsers), true);
+    assert.deepEqual(Object.keys(retUsers), ["userId"]);
+    assert.strictEqual(is.number(retUsers.userId), true);
 });
 
