41ce1d40770df38ef789533c98c28a0a670bc464
Nicolas Marteau <nico_mart@hotmail.fr>
2019-04-17 11:12:46 +0200

    test: add test login()

--- test/test.js
+++ test/test.js
@@ -19,9 +19,32 @@ japa("meta() should returned an Object with uptime Key", async(assert) => {
     assert.deepEqual(Object.keys(retMeta), ["uptime"]);
 });
 
-japa("login() should returned an error if no arguments", async(assert) => {
-    
-})
+japa("login() should returned an ArgumentError if no arguments", async(assert) => {
+    try {
+        await login();
+    }
+    catch (err) {
+        assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+    }
+});
+
+japa("login() should returned an ArgumentError if just one arguments", async(assert) => {
+    try {
+        await login("admin");
+    }
+    catch (err) {
+        assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+    }
+});
+
+japa("login() should returned an ArgumentError if arguments aren't string", async(assert) => {
+    try {
+        await login("admin", 50);
+    }
+    catch (err) {
+        assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+    }
+});
 
 japa("login() should returned a string", async(assert) => {
     const retLogin = await login("admin1", "admin1953");
