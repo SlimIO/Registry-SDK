52c9981492bb377004925a77d8c0a95670d50e7e
Nicolas Marteau <nico_mart@hotmail.fr>
2019-04-17 22:08:49 +0200

    test: add group with before japa

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 // Require Node.js dependencies
 const { writeFile, unlink } = require("fs").promises;
+const { existsSync, unlinkSync } = require("fs");
 const { join } = require("path");
 
 // Require Third-Party dependencies
@@ -14,169 +15,184 @@ const { meta, login, users, addon, addonName, publish, orga, orgaName, orgaAddUs
 // Constantes
 const PRIMITIVES = [{}, 10, true, undefined];
 const PATH = __dirname;
+const PATH_PKG = join(PATH, "package.json");
+const PATH_TOML = join(PATH, "package.json");
 
-japa("require of index.js should returned the set methods", (assert) => {
-    assert.strictEqual(is.plainObject(setOfMethods), true);
-    assert.deepEqual(Object.keys(setOfMethods).sort(), [
-        "meta",
-        "login",
-        "users",
-        "addon",
-        "addonName",
-        "publish",
-        "orga",
-        "orgaName",
-        "orgaAddUser"
-    ].sort());
-});
+japa.group("Test methods", (group) => {
+    group.before(() => {
+        if (existsSync(PATH_PKG)) {
+            unlinkSync(PATH_PKG);
+        }
+        if (existsSync(PATH_TOML)) {
+            unlinkSync(PATH_TOML);
+        }
+    });
 
-japa("meta() should returned an Object with uptime Key (number)", async(assert) => {
-    const retMeta = await meta();
+    japa("require of index.js should returned the set methods", (assert) => {
+        assert.strictEqual(is.plainObject(setOfMethods), true);
+        assert.deepEqual(Object.keys(setOfMethods).sort(), [
+            "meta",
+            "login",
+            "users",
+            "addon",
+            "addonName",
+            "publish",
+            "orga",
+            "orgaName",
+            "orgaAddUser"
+        ].sort());
+    });
 
-    assert.strictEqual(is.plainObject(retMeta), true);
-    assert.deepEqual(Object.keys(retMeta), ["uptime"]);
-    assert.strictEqual(is.number(retMeta.uptime), true);
-});
+    japa("meta() should returned an Object with uptime Key (number)", async(assert) => {
+        const retMeta = await meta();
 
-japa("login() should returned an ArgumentError if argument(s) aren't string", async(assert) => {
-    assert.plan(4);
+        assert.strictEqual(is.plainObject(retMeta), true);
+        assert.deepEqual(Object.keys(retMeta), ["uptime"]);
+        assert.strictEqual(is.number(retMeta.uptime), true);
+    });
 
-    for (const prim of PRIMITIVES) {
+    japa("login() should returned an ArgumentError if argument(s) aren't string", async(assert) => {
+        assert.plan(4);
+
+        for (const prim of PRIMITIVES) {
+            try {
+                await login(prim);
+            }
+            catch (err) {
+                assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+            }
+        }
+    });
+
+    japa("login() should returned a string", async(assert) => {
+        const retLogin = await login("admin1", "admin1953");
+
+        assert.strictEqual(is.string(retLogin), true);
+    });
+
+    japa("users() should returned an ArgumentError if argument(s) aren't string", async(assert) => {
+        assert.plan(4);
+
+        for (const prim of PRIMITIVES) {
+            try {
+                await users(prim);
+            }
+            catch (err) {
+                assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+            }
+        }
+    });
+
+    japa.skip("users() should returned an object with userId key (number)", async(assert) => {
+        const nbUserRandom = Math.floor(Math.random() * 10000);
+        const retUsers = await users(`admin${nbUserRandom}`, "admin1953");
+
+        assert.strictEqual(is.plainObject(retUsers), true);
+        assert.deepEqual(Object.keys(retUsers), ["userId"]);
+        assert.strictEqual(is.number(retUsers.userId), true);
+    });
+
+    japa("addon() should returned an array", async(assert) => {
+        const retAddon = await addon();
+
+        assert.strictEqual(is.array(retAddon), true);
+    });
+
+    japa("addonName() should returned an ArgumentError if argument(s) isn't string", async(assert) => {
+        assert.plan(4);
+
+        for (const prim of PRIMITIVES) {
+            try {
+                await addonName(prim);
+            }
+            catch (err) {
+                assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+            }
+        }
+    });
+
+    japa("addonName() should returned an object", async(assert) => {
+        const retAddonName = await addonName("memory");
+
+        assert.strictEqual(is.plainObject(retAddonName), true);
+        assert.deepEqual(Object.keys(retAddonName).sort(), [
+            "name",
+            "description",
+            "git",
+            "createdAt",
+            "updatedAt",
+            "author",
+            "organisation",
+            "versions"
+        ].sort());
+    });
+
+    japa("publish() should returned an ArgumentError if argument(s) isn't string", async(assert) => {
+        assert.plan(4);
+
+        for (const prim of PRIMITIVES) {
+            try {
+                await publish(prim);
+            }
+            catch (err) {
+                assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+            }
+        }
+    });
+
+    japa("publish() should returned an Error if first argument isn't path of a directory", async(assert) => {
+        assert.plan(1);
+
         try {
-            await login(prim);
+            await publish("ZzzzzzBlaaaaa", "myToken");
         }
-        catch (err) {
-            assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+        catch ({ message }) {
+            assert.strictEqual(message, "The Addon main directory path is incorect");
         }
-    }
-});
+    });
 
-japa("login() should returned a string", async(assert) => {
-    const retLogin = await login("admin1", "admin1953");
+    japa("publish() should returned an Error if package.json doesn't exist", async(assert) => {
+        assert.plan(1);
 
-    assert.strictEqual(is.string(retLogin), true);
-});
-
-japa("users() should returned an ArgumentError if argument(s) aren't string", async(assert) => {
-    assert.plan(4);
-
-    for (const prim of PRIMITIVES) {
         try {
-            await users(prim);
+            await publish(PATH, "myToken");
         }
-        catch (err) {
-            assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+        catch ({ message }) {
+            assert.strictEqual(message, `package.json doesn't exist in "${__dirname}" !`);
         }
-    }
-});
+    });
 
-japa.skip("users() should returned an object with userId key (number)", async(assert) => {
-    const nbUserRandom = Math.floor(Math.random() * 10000);
-    const retUsers = await users(`admin${nbUserRandom}`, "admin1953");
+    japa("publish() should returned an Error if slimio.toml doesn't exist", async(assert) => {
+        assert.plan(1);
 
-    assert.strictEqual(is.plainObject(retUsers), true);
-    assert.deepEqual(Object.keys(retUsers), ["userId"]);
-    assert.strictEqual(is.number(retUsers.userId), true);
-});
-
-japa("addon() should returned an array", async(assert) => {
-    const retAddon = await addon();
-
-    assert.strictEqual(is.array(retAddon), true);
-});
-
-japa("addonName() should returned an ArgumentError if argument(s) isn't string", async(assert) => {
-    assert.plan(4);
-
-    for (const prim of PRIMITIVES) {
+        await writeFile(join(PATH, "package.json"), "");
         try {
-            await addonName(prim);
+            await publish(PATH, "myToken");
         }
-        catch (err) {
-            assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+        catch ({ message }) {
+            assert.strictEqual(message, `slimio.toml doesn't exist in "${PATH}" !`);
         }
-    }
-});
+        await unlink(join(PATH, "package.json"));
+    });
 
-japa("addonName() should returned an object", async(assert) => {
-    const retAddonName = await addonName("memory");
+    japa("publish() should returned an Error if project isn't addon type", async(assert) => {
+        assert.plan(1);
 
-    assert.strictEqual(is.plainObject(retAddonName), true);
-    assert.deepEqual(Object.keys(retAddonName).sort(), [
-        "name",
-        "description",
-        "git",
-        "createdAt",
-        "updatedAt",
-        "author",
-        "organisation",
-        "versions"
-    ].sort());
-});
+        const tomlText = "name = \"test\"\nversion = \"0.1.0\"\ntype = \"Package\"\ndependencies = { }";
+        await writeFile(join(PATH, "package.json"), "");
+        await writeFile(join(PATH, "slimio.toml"), tomlText);
 
-japa("publish() should returned an ArgumentError if argument(s) isn't string", async(assert) => {
-    assert.plan(4);
-
-    for (const prim of PRIMITIVES) {
+        const manifest = await Manifest.open(join(PATH, "slimio.toml"));
         try {
-            await publish(prim);
+            await publish(PATH, "myToken");
         }
-        catch (err) {
-            assert.strictEqual(Reflect.get(err, "name"), "ArgumentError");
+        catch ({ message }) {
+            assert.strictEqual(message, "Your project isn't of addon type");
         }
-    }
+
+        await unlink(join(PATH, "package.json"));
+        await unlink(join(PATH, "slimio.toml"));
+    });
 });
 
-japa("publish() should returned an Error if first argument isn't path of a directory", async(assert) => {
-    assert.plan(1);
 
-    try {
-        await publish("ZzzzzzBlaaaaa", "myToken");
-    }
-    catch ({ message }) {
-        assert.strictEqual(message, "The Addon main directory path is incorect");
-    }
-});
-
-japa("publish() should returned an Error if package.json doesn't exist", async(assert) => {
-    assert.plan(1);
-
-    try {
-        await publish(PATH, "myToken");
-    }
-    catch ({ message }) {
-        assert.strictEqual(message, `package.json doesn't exist in "${__dirname}" !`);
-    }
-});
-
-japa("publish() should returned an Error if slimio.toml doesn't exist", async(assert) => {
-    assert.plan(1);
-
-    await writeFile(join(PATH, "package.json"), "");
-    try {
-        await publish(PATH, "myToken");
-    }
-    catch ({ message }) {
-        assert.strictEqual(message, `slimio.toml doesn't exist in "${PATH}" !`);
-    }
-    await unlink(join(PATH, "package.json"));
-});
-
-japa("publish() should returned an Error if project isn't addon type", async(assert) => {
-    assert.plan(1);
-
-    const tomlText = "name = \"test\"\nversion = \"0.1.0\"\ntype = \"Package\"\ndependencies = { }";
-    await writeFile(join(PATH, "package.json"), "");
-    await writeFile(join(PATH, "slimio.toml"), tomlText);
-
-    const manifest = await Manifest.open(join(PATH, "slimio.toml"));
-    try {
-        await publish(PATH, "myToken");
-    }
-    catch ({ message }) {
-        assert.strictEqual(message, "Your project isn't of addon type");
-    }
-
-    await unlink(join(PATH, "package.json"));
-    await unlink(join(PATH, "slimio.toml"));
-});
